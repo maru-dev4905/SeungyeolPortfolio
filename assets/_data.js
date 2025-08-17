291888c3cb841e0c3f6e1f7b79bc7ffe8ef0de45
marudev4905 <marudev4905@gmail.com>
2025-08-17 18:11:58 +0900

    main animation 수정

---
+++ assets/_data.js
@@ -0,0 +1,22 @@
+export default data = [
+  {
+    project: "SAMWHAPAPER",
+    type: "Interactive Adaptive Web",
+    client: "ADQUA ( via DFRY )",
+    role: "Web Publishing 100%",
+    Period: "1 Month",
+    Skills: ["PUG","SCSS","JAVASCRIPT","JQUERY","SWIPER.JS","SCROLLMAGIC.JS","LUCID.JS","GSAP","FULLPAGE.JS"],
+    awards: ["gdweb", "wa"],
+    message: "제품 감성과 웹 경험을 연결하기 위해 GSAP, ScrollMagic, Locomotive-scroll을 활용해 스크롤 기반 인터랙션과 고급스러운 애니메이션을 구현했습니다. 적응형(Adaptive) 방식으로 PC/모바일 디바이스 별 구조를 분리 설계했습니다."
+  },
+  {
+    project: "Daedong Mobility",
+    type: "Responsive Web",
+    client: "Daedong ( via WeverCNS )",
+    role: "Web Publishing 100%",
+    Period: "1 Month",
+    Skills: ["PUG","SCSS","JAVASCRIPT","JQUERY","SWIPER.JS","SCROLLMAGIC.JS","LUCID.JS","GSAP","FULLPAGE.JS"],
+    awards: ["gdweb", "wa"],
+    message: "제품 감성과 웹 경험을 연결하기 위해 GSAP, ScrollMagic, Locomotive-scroll을 활용해 스크롤 기반 인터랙션과 고급스러운 애니메이션을 구현했습니다. 적응형(Adaptive) 방식으로 PC/모바일 디바이스 별 구조를 분리 설계했습니다."
+  },
+]
--- assets/js/modules/datetime.js
+++ assets/js/modules/datetime.js
@@ -3,7 +3,7 @@ export default function dateTimeModule(){
     const now = new Date;
   
     const year = now.getFullYear();
-    const month = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth()+1).padStart(2, '0');
     const day = String(now.getDate()).padStart(2, '0');
     
     const hour = String(now.getHours()).padStart(2, '0');
