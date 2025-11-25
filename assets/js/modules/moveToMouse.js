e29387d74ad325e60e84ba3de4f780013e36efec
Seungyeol01 <sy@wever.kr>
2025-11-25 18:00:51 +0900

    마우스 포인터 추가

--- assets/js/modules/moveToMouse.js
+++ assets/js/modules/moveToMouse.js
@@ -2,6 +2,77 @@ export default function moveMouseAnimation() {
   const pageX = document.querySelector(".coordinate_display_x");
   const pageY = document.querySelector(".coordinate_display_y");
 
+  gsap.set('.cursor-dot', {scale: 0.1})
+  gsap.set('.cursor-outline', {scale: 0.5})
+
+  document.addEventListener("mousemove", mouseMove);
+
+  let xCTo = gsap.quickTo(".cursor-outline", "left", {
+    duration: 0.2,
+    ease: "power3"
+  });
+  let yCTo = gsap.quickTo(".cursor-outline", "top", {
+    duration: 0.2,
+    ease: "power3"
+  });
+
+  let xDTo = gsap.quickTo(".cursor-dot", "left", {
+    duration: 0.6,
+    ease: "power3"
+  });
+  let yDTo = gsap.quickTo(".cursor-dot", "top", {
+    duration: 0.6,
+    ease: "power3"
+  });
+
+  let isVisible = false;
+
+  function mouseMove(e) {
+    if (!isVisible) {
+      gsap.set(".cursor-outline, .cursor-dot", { opacity: 1 });
+      isVisible = true;
+    }
+
+    const cursorPosition = {
+      left: e.clientX - 40,
+      top: e.clientY - 40
+    };
+
+    xCTo(cursorPosition.left);
+    yCTo(cursorPosition.top);
+    xDTo(cursorPosition.left);
+    yDTo(cursorPosition.top);
+  }
+
+  let targets = gsap.utils.toArray(".target");
+
+  let scaleAnim = gsap.timeline({paused: true});
+
+  scaleAnim
+      .to(".cursor-outline", {
+        scale: 1
+      })
+      .to(
+          ".cursor-dot",
+          {
+            scale: 1,
+            duration: 0.35
+          },
+          0
+      );
+
+  targets.forEach((target) => {
+    target.addEventListener("mouseenter", (e) => {
+      console.log('play')
+      scaleAnim.play();
+    });
+
+    target.addEventListener("mouseleave", (e) => {
+      console.log('reverse')
+      scaleAnim.reverse();
+    });
+  });
+
   const updateDisplay = (e) => {
     pageX.innerText = `X: ${e.pageX}px`;
     pageY.innerText = `Y: ${e.pageY}px`;
