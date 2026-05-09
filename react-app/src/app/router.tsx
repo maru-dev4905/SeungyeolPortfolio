846ae9be3c6e61b86c8688446959ef24759d3e97
maru-dev4905 <marudev4905@gmail.com>
2026-05-10 02:50:41 +0900

    .

--- react-app/src/app/router.tsx
+++ react-app/src/app/router.tsx
@@ -1,23 +1,23 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { createBrowserRouter } from "react-router-dom";
 
-import { AppLayout } from '../components/layout/AppLayout'
-import { AboutPage } from '../pages/AboutPage'
-import { ContactPage } from '../pages/ContactPage'
-import { HomePage } from '../pages/HomePage'
-import { WorkDetailPage } from '../pages/WorkDetailPage'
-import { WorksPage } from '../pages/WorksPage'
+import { AppLayout } from "../components/layout/AppLayout";
+import { AboutPage } from "../pages/AboutPage";
+import { ContactPage } from "../pages/ContactPage";
+import { HomePage } from "../pages/HomePage";
+import { WorkDetailPage } from "../pages/WorkDetailPage";
+import { WorksPage } from "../pages/WorksPage";
 
 export const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <AppLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'about', element: <AboutPage /> },
-      { path: 'works', element: <WorksPage /> },
-      { path: 'works/:projectSlug', element: <WorkDetailPage /> },
-      { path: 'work', element: <WorkDetailPage /> },
-      { path: 'contact', element: <ContactPage /> },
+      { path: "about", element: <AboutPage /> },
+      { path: "works", element: <WorksPage /> },
+      { path: "works/:projectSlug", element: <WorkDetailPage /> },
+      { path: "work", element: <WorkDetailPage /> },
+      { path: "contact", element: <ContactPage /> },
     ],
   },
-])
+]);
